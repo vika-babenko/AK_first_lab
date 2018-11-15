de456f469f30d387e3ce54d053dabd1ccf82e3a4
Sergii Piatakov <sergii.piatakov@globallogic.com>
2018-11-15 15:43:25 +0200

    add a multiplication operation
    Add a member-function which performs multiplication of two float point
    digits.
    
    Signed-off-by: Sergii Piatakov <sergii.piatakov@globallogic.com>

--- calculator.h
+++ calculator.h
@@ -1,11 +1,21 @@
 #ifndef CALCULATOR_H
 #define CALCULATOR_H
 
+
 class Calculator
 {
     public:
+<<<<<<< HEAD
         int Add (double, double);
         int Sub (double, double);
+=======
+        int Add (int, int);
+        int Mul (double, double);
+>>>>>>> e0ea21b (add a multiplication operation)
 };
+// It is first lab
+// Student: Victoria babenko
+// Group: IV-92
+
 
 #endif//CALCULATOR_H
